757949a6f53820d56883c1920c08e304a2ff3053
V3y64 <goldcarexpresso@gmail.com>
2026-08-08 23:44:06 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import {View,Text} from 'react-native';
+export default function App(){
+ return <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
+ <Text>AURORA - Projeto Base</Text>
+ </View>;
+}
