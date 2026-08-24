03f374d119a4e4eacad65d9c7a6f9558427c5750
V3y64 <goldcarexpresso@gmail.com>
2026-08-24 01:53:43 -0300

    Import Notificar in Alertas

--- src/Alertas.jsx
+++ src/Alertas.jsx
@@ -1,5 +1,6 @@
 import { useCallback, useEffect, useState } from 'react'
 import { supabase } from './supabase.js'
+import Notificar from './Notificar.jsx'
 
 const SELECAO = `
   id, autor_id, situacao, mensagem, latitude, longitude,
